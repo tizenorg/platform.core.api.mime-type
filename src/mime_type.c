cd105dadccc38eee9a94313b7b09a82ca40d0360
SukHyung, Kang <shine.kang@samsung.com>
2015-12-09 15:28:23 +0900

    change code to apply coding rule and remove TC codes
    Change-Id: Iaa5aaba941fd056317636e6b9c9e2025868b38e6
    Signed-off-by: SukHyung, Kang <shine.kang@samsung.com>

--- src/mime_type.c
+++ src/mime_type.c
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an AS IS BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 #include <stdio.h>
@@ -33,8 +33,7 @@
 
 static const char* mime_type_error_to_string(mime_type_error_e error)
 {
-	switch (error)
-	{
+	switch (error) {
 	case MIME_TYPE_ERROR_NONE:
 		return "NONE";
 
@@ -47,7 +46,7 @@ static const char* mime_type_error_to_string(mime_type_error_e error)
 	case MIME_TYPE_ERROR_IO_ERROR:
 		return "IO_ERROR";
 
-	default :
+	default:
 		return "UNKNOWN";
 	}
 }
@@ -55,13 +54,9 @@ static const char* mime_type_error_to_string(mime_type_error_e error)
 static int mime_type_error(mime_type_error_e error, const char* function, const char *description)
 {
 	if (description)
-	{
-		LOGE("[%s] %s(0x%08x) : %s", function, mime_type_error_to_string(error), error, description);	
-	}
+		LOGE("[%s] %s(0x%08x) : %s", function, mime_type_error_to_string(error), error, description);
 	else
-	{
-		LOGE("[%s] %s(0x%08x)", function, mime_type_error_to_string(error), error);	
-	}
+		LOGE("[%s] %s(0x%08x)", function, mime_type_error_to_string(error), error);
 
 	return error;
 }
@@ -73,41 +68,31 @@ int mime_type_get_mime_type(const char *file_extension, char **mime_type)
 	const char *xdg_mime_type = NULL;
 
 	if (file_extension == NULL || mime_type == NULL)
-	{
 		return mime_type_error(MIME_TYPE_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
-	}
 
 	if (file_extension[0] == '\0')
-	{
 		return mime_type_error(MIME_TYPE_ERROR_INVALID_PARAMETER, __FUNCTION__, "the given file extension is an empy string");
-	}
 
 	if (file_extension[0] == FILE_EXTENSION_DELIMITER)
-	{
 		return mime_type_error(MIME_TYPE_ERROR_INVALID_PARAMETER, __FUNCTION__, "the given file extension includes the leading dot ('.')");
-	}
 
 	snprintf(file_extension_with_dot, sizeof(file_extension_with_dot), ".%s", file_extension);
 
 	xdg_mime_type = xdg_mime_get_mime_type_from_file_name(file_extension_with_dot);
 
-	if (xdg_mime_type == NULL || *xdg_mime_type == '\0')
-	{
+	if (xdg_mime_type == NULL || *xdg_mime_type == '\0') {
 		*mime_type = NULL;
 		return mime_type_error(MIME_TYPE_ERROR_IO_ERROR, __FUNCTION__, "failed to get the mime type from the shared MIME database");
 	}
-	
+
 	char * mime_type_dup = NULL;
 
 	mime_type_dup = strdup(xdg_mime_type);
 
 	if (mime_type_dup == NULL)
-	{
 		return mime_type_error(MIME_TYPE_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
-	}
 
 	*mime_type = mime_type_dup;
-	
 
 	return MIME_TYPE_ERROR_NONE;
 }
@@ -120,42 +105,30 @@ int mime_type_get_file_extension(const char *mime_type, char *** file_extension,
 	int file_extension_array_index = 0;
 
 	if (mime_type == NULL || file_extension == NULL || length == NULL)
-	{
 		return mime_type_error(MIME_TYPE_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
-	}
 
 	if (mime_type[0] == '\0')
-	{
 		return mime_type_error(MIME_TYPE_ERROR_INVALID_PARAMETER, __FUNCTION__, "the given MIME type is an empy string");
-	}
 
 	xdg_file_extension = xdg_mime_get_file_names_from_mime_type(mime_type);
 
 	if (xdg_file_extension == NULL)
-	{
 		return mime_type_error(MIME_TYPE_ERROR_IO_ERROR, __FUNCTION__, "failed to get the file extensions from the shared MIME database");
-	}
 
 	while (xdg_file_extension[xdg_file_extension_length] != NULL)
-	{
 		xdg_file_extension_length++;
-	}
 
 	file_extension_array = calloc(xdg_file_extension_length, sizeof(char*));
-	
-	if (file_extension_array == NULL)
-	{
-		return mime_type_error(MIME_TYPE_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
-	}
 
-	for (xdg_file_extension_length = 0; xdg_file_extension[xdg_file_extension_length] != NULL; xdg_file_extension_length++)
-	{
+	if (file_extension_array == NULL)
+		return mime_type_error(MIME_TYPE_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
+
+	for (xdg_file_extension_length = 0; xdg_file_extension[xdg_file_extension_length] != NULL; xdg_file_extension_length++) {
 		char *entry = NULL;
-		
+
 		entry = strrchr(xdg_file_extension[xdg_file_extension_length], FILE_EXTENSION_DELIMITER);
 
-		if (entry != NULL)
-		{
+		if (entry != NULL) {
 			entry++;
 			file_extension_array[file_extension_array_index] = strdup(entry);
 			file_extension_array_index++;
