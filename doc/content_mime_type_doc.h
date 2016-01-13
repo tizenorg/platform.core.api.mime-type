5d41bd2425db68a2bf733a5a00b2a82427c381f3
SukHyung, Kang <shine.kang@samsung.com>
2016-01-14 08:12:32 +0900

    Add doc file for API references
    Change-Id: I9d17a022c05bc6e486c671ddd3b03bc9e7fb58b1
    Signed-off-by: SukHyung, Kang <shine.kang@samsung.com>

---
+++ doc/content_mime_type_doc.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+#ifndef __TIZEN_CONTENT_MIME_TYPE_DOC_H__
+#define __TIZEN_CONTENT_MIME_TYPE_DOC_H__
+
+/**
+ * @ingroup CAPI_CONTENT_FRAMEWORK
+ * @defgroup CAPI_CONTENT_MIME_TYPE_MODULE MIME Type
+ * @brief The @ref CAPI_CONTENT_MIME_TYPE_MODULE API provides functions to map MIME types to file extensions and vice versa.
+ *
+ * @section CAPI_CONTENT_MIME_TYPE_MODULE_HEADER Required Header
+ *   \#include <mime_type.h>
+ *
+ * @section CAPI_CONTENT_MIME_TYPE_MODULE_OVERVIEW Overview
+ * The MIME Type API provides functions to map MIME types to file extensions and vice versa.
+ * Conversions are provided from file extensions to MIME types and from MIME types to file extensions. \n
+ * Note that some MIME types have multiple file extensions or no extension,
+ * and the MIME type is 'application/octet-stream' if the given file extension is not associated with specific file formats
+ */
+
+#endif /* __TIZEN_CONTENT_MIME_TYPE_DOC_H__ */
+
